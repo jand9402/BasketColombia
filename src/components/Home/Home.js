777be71210eaa6c8cb8a40a9a58e822d5429be50
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-06-11 19:33:47 -0500

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Slides from "./Slides/Slides";
+
+function Home(){
+return(
+    <Slides/> 
+)
+}
+
+export default Home
